5d80b94ebba205840f52a263be01013ecd8f9035
JungHuNi <calendar8342@naver.com>
2019-05-28 19:01:17 +0900

    first commit

---
+++ js/menu_util.js
@@ -0,0 +1,29 @@
+
+/**
+	@param src 현재 페이지에서 이동할 페이지 경로
+*/
+function onChangeLocation(src) {
+	window.location.href = src;
+}
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "block";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
